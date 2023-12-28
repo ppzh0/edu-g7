0fa21fb12cb07e16e81ac7362accf9e7f39e5cf4
Pancake <narrtor06@gmail.com>
2023-12-28 22:52:02 +0630

    added files

---
+++ main.js
@@ -0,0 +1,26 @@
+let about = `ABOUT THIS WEBSITE
+
+    This website is use to view or download grade 7 pdf files!
+    Time spent: 12 days
+    Made by ppzh0.`;
+
+
+let report = `  REPORT A PROBLEM
+
+Want to report a problem or something unusual?
+Contact me on 
+    Discord:    pancake#2493
+    Email:       narrtor06@gmail.com
+`;
+
+
+let coffee = 
+`BUY ME A COFFEE
+
+Oh, that is no need, you can help me by sharing this website!`;
+
+function opoo() {
+    window.open('https://t.me/+jGAeGghbeWg0NjE1', 'telegram', 'location=1,status=1,scrollbars=1, width=450,height=678, left=450');
+}
+
+let dev = "Entering logs. [devs only]"
